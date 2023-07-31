6dbea8fac42c68d5107efc1f6aec88d77c046dea
landandair <jonhuwaldt@gmail.com>
2023-07-31 18:43:12 -0500

    Added network client for Command client

--- Server/start_server.py
+++ Server/start_server.py
@@ -5,4 +5,7 @@ from Server_Client_Data import ServerData
 if __name__ == '__main__':
     corner_pos = (100, 100)
     file_name = 'Templates/template.png'
-    main(ServerData(file_name, corner_pos))
+    try:
+        main(ServerData(file_name, corner_pos))
+    except KeyboardInterrupt:
+        print('Closing Server...')
